bf398e4ac654550c18f9afd01941470ff0852069
thesavant42 <jbrashars@gmail.com>
2025-04-25 19:16:24 -0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,27 @@
+# config.py
+# Pin configuration for ZS-X11H motor tester
+# Author: savant42
+import board
+# Define the pin mappings for the motors
+MOTOR_CONFIG = {
+    'LEFT': {
+        'name': 'Left Wheel',
+        'PWM': board.D13,       # aka IO11
+        'DIR': board.D9,        # aka IO1
+        'STOP': board.D10,      # aka IO3
+        'BRAKE': board.D6,      # aka IO38
+        'PULSE': board.D12,     # aka IO10
+        'FWD': True,
+        'DESIRED_DIR': 'FWD'
+    },
+    'RIGHT': {
+        'name': 'Right Wheel',
+        'PWM': board.D19,       # aka IO5
+        'DIR': board.D16,       # aka IO14
+        'STOP': board.D17,      # aka IO12
+        'BRAKE': board.D15,     # aka IO18
+        'PULSE': board.D14,     # aka IO17
+        'FWD': False,
+        'DESIRED_DIR': 'FWD'
+    }
+}    
